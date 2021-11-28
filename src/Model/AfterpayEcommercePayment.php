fd4d850fe9f8afd8c7cef3bb08255443278aaea8
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-11-29 09:40:08 +1300

    WIP

--- src/Model/AfterpayEcommercePayment.php
+++ src/Model/AfterpayEcommercePayment.php
@@ -92,7 +92,7 @@ class AfterpayEcommercePayment extends EcommercePayment
      */
     public function processPayment($data, Form $form)
     {
-        $order = $this->Order();
+        $order = $this->orderCached();
         $token = $this->getTokenFromAfterpay($order);
 
         return $this->executeURL($token);
